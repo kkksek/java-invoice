840e084a363ab7587ed62ccc3d2f61c0e102a506
Katarzyna Sek <ksek@student.agh.edu.pl>
2023-02-11 15:56:03 +0100

    Whole products corrected + some invoices

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,24 +2,25 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private HashMap<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        this.products.put(product,1);
     }
 
     public void addProduct(Product product, Integer quantity) {
         // TODO: implement
     }
 
-    public BigDecimal getSubtotal() {
+    public BigDecimal getSubtotal() {//zmienic nazwe jesli niejasna
         BigDecimal subtotal = new BigDecimal("0");
         if (!(products==null||products.isEmpty())) {
-            for (Product product : this.products) {
+            for (Product product : this.products.keySet()) {
                 subtotal = subtotal.add(product.getPrice());
             }
         }
@@ -29,14 +30,14 @@ public class Invoice {
     public BigDecimal getTax() {
         BigDecimal tax = new BigDecimal("0");
         if (!(products==null||products.isEmpty())) {
-            for (Product product : this.products) {
-                tax = tax.add(product.getTaxPercent());
+            for (Product product : this.products.keySet()) {
+                tax = tax.add(product.getTaxPercent().multiply(product.getPrice()));
             }
         }
         return tax;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        return getTax().add(getSubtotal());
     }
 }
