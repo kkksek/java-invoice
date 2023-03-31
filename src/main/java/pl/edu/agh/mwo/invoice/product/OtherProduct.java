d830acdcfcc933010a40ab89dac51a920978fd43
Panda <ksek@student.agh.edu.pl>
2023-03-31 17:41:35 +0200

    IC-2022.2.4 <Panda@LAPTOP-4FJJ1GSN Overwrite remote https://github.com/kkksek/java-invoice to local

--- src/main/java/pl/edu/agh/mwo/invoice/product/OtherProduct.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.mwo.invoice.product;
-
-import java.math.BigDecimal;
-
-public class OtherProduct extends Product {
-    public OtherProduct(String name, BigDecimal price) {
-        super(name, price, new BigDecimal("0.23"));
-    }
-}
--- src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.mwo.invoice.product;
-
-import java.math.BigDecimal;
-
-public class TaxFreeProduct extends Product {
-    public TaxFreeProduct(String name, BigDecimal price) {
-        super(name, price, BigDecimal.ZERO);
-    }
-}
